e079538202581248fdbef3b0609ec87707d4deb8
ILikeRice333 <miles.ian.javate@gmail.com>
2026-05-19 14:16:48 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -89,3 +89,47 @@ export default function App() {
     </div>
   );
 }
+{/* floating mustard bottle images */}
+<img
+  src="https://cdn-icons-png.flaticon.com/512/3143/3143643.png"
+  alt="mustard"
+  style={{
+    position: "absolute",
+    top: "8%",
+    right: "30%",
+    width: "55px"
+  }}
+/>
+
+<img
+  src="https://cdn-icons-png.flaticon.com/512/3143/3143643.png"
+  alt="mustard"
+  style={{
+    position: "absolute",
+    top: "35%",
+    left: "8%",
+    width: "55px"
+  }}
+/>
+
+<img
+  src="https://cdn-icons-png.flaticon.com/512/3143/3143643.png"
+  alt="mustard"
+  style={{
+    position: "absolute",
+    bottom: "22%",
+    right: "8%",
+    width: "55px"
+  }}
+/>
+
+<img
+  src="https://cdn-icons-png.flaticon.com/512/3143/3143643.png"
+  alt="mustard"
+  style={{
+    position: "absolute",
+    bottom: "5%",
+    left: "35%",
+    width: "55px"
+  }}
+/>
